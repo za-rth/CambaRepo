b790ad61611d25169303fbc19d402023f9a73f5a
zarth <98376279+za-rth@users.noreply.github.com>
2024-10-01 00:29:33 +0800

    ADDED PROFILE ROUTES

--- resources/views/splash/heading.blade.php
+++ resources/views/splash/heading.blade.php
@@ -1,3 +1,4 @@
+
 @section('header')
 @section('header2')
 <header class="navbar navbar-expand-lg navbar-light bg-light">
@@ -10,7 +11,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="{{route('login')}}">Login</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
